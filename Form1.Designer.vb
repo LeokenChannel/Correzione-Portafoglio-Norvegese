c35e21a35e082e8f43256321f62805cb15dfefb3
Leo Garoli <leo.garoli@live.it>
2021-06-17 19:39:21 +0200

    Variazioni concluse.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,67 +24,98 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Txt_Percorso_Anagist_Corretto = New System.Windows.Forms.TextBox()
+        Me.Btn_Percorso_Anagist = New System.Windows.Forms.Button()
+        Me.Btn_Percorso_AnaSog_Completa = New System.Windows.Forms.Button()
+        Me.Txt_Percorso_Anagist = New System.Windows.Forms.TextBox()
+        Me.Txt_Percorso_Anasog_Completa = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Lbl_SelezionePercorso_Anagist = New System.Windows.Forms.Label()
         Me.Lbl_SelezionePercorso_AnaSog_Completa = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Btn_AvviaCorrezione = New System.Windows.Forms.Button()
         Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
         Me.Grp_LogElaborazioni = New System.Windows.Forms.GroupBox()
         Me.Lbl_Elaborazioni = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Lbl_CFPIVA_Elaborato = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.Grp_LogElaborazioni.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Button3)
-        Me.GroupBox1.Controls.Add(Me.Button2)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Txt_Percorso_Anagist_Corretto)
+        Me.GroupBox1.Controls.Add(Me.Btn_Percorso_Anagist)
+        Me.GroupBox1.Controls.Add(Me.Btn_Percorso_AnaSog_Completa)
+        Me.GroupBox1.Controls.Add(Me.Txt_Percorso_Anagist)
+        Me.GroupBox1.Controls.Add(Me.Txt_Percorso_Anasog_Completa)
+        Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.Lbl_SelezionePercorso_Anagist)
         Me.GroupBox1.Controls.Add(Me.Lbl_SelezionePercorso_AnaSog_Completa)
         Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(1074, 274)
+        Me.GroupBox1.Size = New System.Drawing.Size(1074, 271)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Selezione archivi"
+        Me.GroupBox1.Text = "Selezione archivi per la corrrezione di un Portafoglio"
         '
-        'Button3
+        'Txt_Percorso_Anagist_Corretto
         '
-        Me.Button3.Location = New System.Drawing.Point(1013, 175)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(50, 27)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = ". . ."
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Txt_Percorso_Anagist_Corretto.Enabled = False
+        Me.Txt_Percorso_Anagist_Corretto.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.Txt_Percorso_Anagist_Corretto.Location = New System.Drawing.Point(7, 228)
+        Me.Txt_Percorso_Anagist_Corretto.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Txt_Percorso_Anagist_Corretto.Name = "Txt_Percorso_Anagist_Corretto"
+        Me.Txt_Percorso_Anagist_Corretto.Size = New System.Drawing.Size(1057, 26)
+        Me.Txt_Percorso_Anagist_Corretto.TabIndex = 3
+        Me.ToolTip1.SetToolTip(Me.Txt_Percorso_Anagist_Corretto, "Il nome non è modiificabile")
         '
-        'Button2
+        'Btn_Percorso_Anagist
         '
-        Me.Button2.Location = New System.Drawing.Point(1014, 62)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(50, 27)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = ". . ."
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Btn_Percorso_Anagist.Location = New System.Drawing.Point(1013, 134)
+        Me.Btn_Percorso_Anagist.Name = "Btn_Percorso_Anagist"
+        Me.Btn_Percorso_Anagist.Size = New System.Drawing.Size(50, 27)
+        Me.Btn_Percorso_Anagist.TabIndex = 3
+        Me.Btn_Percorso_Anagist.Text = ". . ."
+        Me.Btn_Percorso_Anagist.UseVisualStyleBackColor = True
         '
-        'TextBox2
+        'Btn_Percorso_AnaSog_Completa
         '
-        Me.TextBox2.Location = New System.Drawing.Point(7, 175)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(1000, 26)
-        Me.TextBox2.TabIndex = 1
+        Me.Btn_Percorso_AnaSog_Completa.Location = New System.Drawing.Point(1014, 62)
+        Me.Btn_Percorso_AnaSog_Completa.Name = "Btn_Percorso_AnaSog_Completa"
+        Me.Btn_Percorso_AnaSog_Completa.Size = New System.Drawing.Size(50, 27)
+        Me.Btn_Percorso_AnaSog_Completa.TabIndex = 1
+        Me.Btn_Percorso_AnaSog_Completa.Text = ". . ."
+        Me.Btn_Percorso_AnaSog_Completa.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'Txt_Percorso_Anagist
         '
-        Me.TextBox1.Location = New System.Drawing.Point(7, 62)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(1000, 26)
-        Me.TextBox1.TabIndex = 1
+        Me.Txt_Percorso_Anagist.Location = New System.Drawing.Point(7, 134)
+        Me.Txt_Percorso_Anagist.Name = "Txt_Percorso_Anagist"
+        Me.Txt_Percorso_Anagist.Size = New System.Drawing.Size(1000, 26)
+        Me.Txt_Percorso_Anagist.TabIndex = 2
+        '
+        'Txt_Percorso_Anasog_Completa
+        '
+        Me.Txt_Percorso_Anasog_Completa.Location = New System.Drawing.Point(7, 62)
+        Me.Txt_Percorso_Anasog_Completa.Name = "Txt_Percorso_Anasog_Completa"
+        Me.Txt_Percorso_Anasog_Completa.Size = New System.Drawing.Size(1000, 26)
+        Me.Txt_Percorso_Anasog_Completa.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(5, 201)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(410, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Percorso e nome archivio del Nuovo Anagist corretto:"
         '
         'Lbl_SelezionePercorso_Anagist
         '
@@ -92,11 +123,11 @@ Partial Class Form1
         Me.Lbl_SelezionePercorso_Anagist.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.Lbl_SelezionePercorso_Anagist.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Lbl_SelezionePercorso_Anagist.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lbl_SelezionePercorso_Anagist.Location = New System.Drawing.Point(6, 147)
+        Me.Lbl_SelezionePercorso_Anagist.Location = New System.Drawing.Point(6, 106)
         Me.Lbl_SelezionePercorso_Anagist.Name = "Lbl_SelezionePercorso_Anagist"
-        Me.Lbl_SelezionePercorso_Anagist.Size = New System.Drawing.Size(263, 22)
+        Me.Lbl_SelezionePercorso_Anagist.Size = New System.Drawing.Size(420, 22)
         Me.Lbl_SelezionePercorso_Anagist.TabIndex = 0
-        Me.Lbl_SelezionePercorso_Anagist.Text = "Seleziona archivio di tipo: Anagist"
+        Me.Lbl_SelezionePercorso_Anagist.Text = "Seleziona percorso archivio di tipo: Anagist (Depurato)"
         '
         'Lbl_SelezionePercorso_AnaSog_Completa
         '
@@ -106,19 +137,19 @@ Partial Class Form1
         Me.Lbl_SelezionePercorso_AnaSog_Completa.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Lbl_SelezionePercorso_AnaSog_Completa.Location = New System.Drawing.Point(6, 34)
         Me.Lbl_SelezionePercorso_AnaSog_Completa.Name = "Lbl_SelezionePercorso_AnaSog_Completa"
-        Me.Lbl_SelezionePercorso_AnaSog_Completa.Size = New System.Drawing.Size(345, 22)
+        Me.Lbl_SelezionePercorso_AnaSog_Completa.Size = New System.Drawing.Size(416, 22)
         Me.Lbl_SelezionePercorso_AnaSog_Completa.TabIndex = 0
-        Me.Lbl_SelezionePercorso_AnaSog_Completa.Text = "Seleziona archivio di tipo: AnaSog_Completa"
+        Me.Lbl_SelezionePercorso_AnaSog_Completa.Text = "Seleziona percorso archivio di tipo: AnaSog_Completa"
         '
-        'Button1
+        'Btn_AvviaCorrezione
         '
-        Me.Button1.Font = New System.Drawing.Font("Arial Black", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(62, 293)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(365, 75)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Avvia processo di correzione"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Btn_AvviaCorrezione.Font = New System.Drawing.Font("Arial Black", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn_AvviaCorrezione.Location = New System.Drawing.Point(62, 293)
+        Me.Btn_AvviaCorrezione.Name = "Btn_AvviaCorrezione"
+        Me.Btn_AvviaCorrezione.Size = New System.Drawing.Size(365, 90)
+        Me.Btn_AvviaCorrezione.TabIndex = 4
+        Me.Btn_AvviaCorrezione.Text = "Avvia processo di correzione"
+        Me.Btn_AvviaCorrezione.UseVisualStyleBackColor = True
         '
         'NotifyIcon1
         '
@@ -127,10 +158,12 @@ Partial Class Form1
         '
         'Grp_LogElaborazioni
         '
+        Me.Grp_LogElaborazioni.Controls.Add(Me.Label2)
+        Me.Grp_LogElaborazioni.Controls.Add(Me.Lbl_CFPIVA_Elaborato)
         Me.Grp_LogElaborazioni.Controls.Add(Me.Lbl_Elaborazioni)
-        Me.Grp_LogElaborazioni.Location = New System.Drawing.Point(489, 290)
+        Me.Grp_LogElaborazioni.Location = New System.Drawing.Point(470, 293)
         Me.Grp_LogElaborazioni.Name = "Grp_LogElaborazioni"
-        Me.Grp_LogElaborazioni.Size = New System.Drawing.Size(597, 77)
+        Me.Grp_LogElaborazioni.Size = New System.Drawing.Size(616, 94)
         Me.Grp_LogElaborazioni.TabIndex = 2
         Me.Grp_LogElaborazioni.TabStop = False
         Me.Grp_LogElaborazioni.Text = "Log di elaborazione"
@@ -141,19 +174,41 @@ Partial Class Form1
         Me.Lbl_Elaborazioni.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.Lbl_Elaborazioni.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Lbl_Elaborazioni.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lbl_Elaborazioni.Location = New System.Drawing.Point(6, 33)
+        Me.Lbl_Elaborazioni.Location = New System.Drawing.Point(16, 29)
         Me.Lbl_Elaborazioni.Name = "Lbl_Elaborazioni"
         Me.Lbl_Elaborazioni.Size = New System.Drawing.Size(22, 20)
         Me.Lbl_Elaborazioni.TabIndex = 0
         Me.Lbl_Elaborazioni.Text = "..."
         '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Lbl_CFPIVA_Elaborato
+        '
+        Me.Lbl_CFPIVA_Elaborato.AutoSize = True
+        Me.Lbl_CFPIVA_Elaborato.Location = New System.Drawing.Point(415, 66)
+        Me.Lbl_CFPIVA_Elaborato.Name = "Lbl_CFPIVA_Elaborato"
+        Me.Lbl_CFPIVA_Elaborato.Size = New System.Drawing.Size(62, 17)
+        Me.Lbl_CFPIVA_Elaborato.TabIndex = 1
+        Me.Lbl_CFPIVA_Elaborato.Text = "nessuno"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(129, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(286, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Codice Fiscale o Partita IVA in elaborazione:"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1098, 376)
+        Me.ClientSize = New System.Drawing.Size(1098, 394)
         Me.Controls.Add(Me.Grp_LogElaborazioni)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Btn_AvviaCorrezione)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
         Me.Text = "Correzione Portafoglio Norvegese"
@@ -166,14 +221,20 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Txt_Percorso_Anagist As TextBox
+    Friend WithEvents Txt_Percorso_Anasog_Completa As TextBox
     Friend WithEvents Lbl_SelezionePercorso_Anagist As Label
     Friend WithEvents Lbl_SelezionePercorso_AnaSog_Completa As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Btn_AvviaCorrezione As Button
     Friend WithEvents NotifyIcon1 As NotifyIcon
     Friend WithEvents Grp_LogElaborazioni As GroupBox
     Friend WithEvents Lbl_Elaborazioni As Label
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents Btn_Percorso_Anagist As Button
+    Friend WithEvents Btn_Percorso_AnaSog_Completa As Button
+    Friend WithEvents Txt_Percorso_Anagist_Corretto As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents Lbl_CFPIVA_Elaborato As Label
+    Friend WithEvents Label2 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,269 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Xml
+Imports System.Xml.Linq
+
+Public Class Form1
+    Private Sub Btn_Percorso_AnaSog_Completa_Click(sender As Object, e As EventArgs) Handles Btn_Percorso_AnaSog_Completa.Click
+        Dim NomeFile As String = Seleziona_Percorso(Lbl_SelezionePercorso_AnaSog_Completa.Text)
+
+        If NomeFile <> "" Then
+
+            Txt_Percorso_Anasog_Completa.Text = NomeFile
+        Else
+            Txt_Percorso_Anasog_Completa.Text = ""
+        End If
+    End Sub
+
+    Private Sub Btn_Percorso_Anagist_Click(sender As Object, e As EventArgs) Handles Btn_Percorso_Anagist.Click
+        Dim NomeFile As String = Seleziona_Percorso(Lbl_SelezionePercorso_Anagist.Text)
+
+        If NomeFile <> "" Then
+
+            Txt_Percorso_Anagist.Text = NomeFile
+        Else
+            Txt_Percorso_Anagist.Text = ""
+        End If
+    End Sub
+
+    Function Seleziona_Percorso(ByVal Contesto As String) As String
+        Dim path As String = ""
+        OpenFileDialog1.Title = Me.Text
+        If Contesto <> "" Then
+            OpenFileDialog1.Title = Me.Text & " - " & Contesto
+        End If
+        OpenFileDialog1.FileName = ""
+        OpenFileDialog1.DefaultExt = "txt"
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+        ' Test result.
+        If result = Windows.Forms.DialogResult.OK Then
+
+            ' Get the file name.
+            path = OpenFileDialog1.FileName
+
+        End If
+
+        Seleziona_Percorso = path
+    End Function
+
+    Private Sub Btn_AvviaCorrezione_Click(sender As Object, e As EventArgs) Handles Btn_AvviaCorrezione.Click
+
+        If MessageBox.Show("Si vuole avviare la correzione dell'archivio di Portafoglio selezionato?", Me.Name, MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+            If ControlloFileDefiniti() = True Then
+                If ElaboraCorrezione() = True Then
+                    MessageBox.Show("La correzione del Portafoglio indicato è stata eseguita correttamente", Me.Text)
+
+                    Lbl_Elaborazioni.Text = "Operazione precedente conclusa"
+                    Lbl_CFPIVA_Elaborato.Text = "nessuno"
+
+                    Txt_Percorso_Anasog_Completa.Text = ""
+                    Txt_Percorso_Anagist.Text = ""
+                    Txt_Percorso_Anagist_Corretto.Text = ""
+
+                    Txt_Percorso_Anasog_Completa.Select()
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Function ControlloFileDefiniti() As Boolean
+        Dim EsitoControllo As Boolean = False
+        If My.Computer.FileSystem.FileExists(Txt_Percorso_Anasog_Completa.Text) = False Or
+            My.Computer.FileSystem.FileExists(Txt_Percorso_Anagist.Text) = False Then
+            DialogResult = MessageBox.Show("Gli archivi dei portafogli non sono stati indicati correttamente", Btn_AvviaCorrezione.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+        Else
+
+            EsitoControllo = True
+
+        End If
+
+        Return EsitoControllo
+
+    End Function
+
+    Function ElaboraCorrezione() As Boolean
+        Dim EsitoCorrezione As Boolean = True
+        Dim File_XML_DatiMancanti As String
+
+        Try
+            Lbl_Elaborazioni.Text = "Inizio creazione archivio XML..."
+            File_XML_DatiMancanti = Scansiona_AnaSogCompleta()
+            Lbl_Elaborazioni.Text = "Creazione archivio XML: " & File_XML_DatiMancanti & "avvenuta con successo"
+            Me.Grp_LogElaborazioni.Refresh()
+            EsitoCorrezione = Correggi_Anagist(File_XML_DatiMancanti)
+
+
+        Catch ex As Exception
+
+            EsitoCorrezione = False
+
+        End Try
+
+        Return EsitoCorrezione
+    End Function
+
+    Function Scansiona_AnaSogCompleta() As String
+        Dim FileReader As System.IO.StreamReader
+
+        Dim stringReader As String = ""
+
+        Dim CFoPIVA As String = ""
+        Dim NoLuogoNascita As String = ""
+        Dim NoSedeLegale As String = ""
+        Dim NoSpecieGiuridica As String = ""
+        Dim NoCodiceCCIA As String = ""
+
+        '----------------------------------------------------------------------------------------- AnaSogCompleta
+        FileReader = My.Computer.FileSystem.OpenTextFileReader(Txt_Percorso_Anasog_Completa.Text)
+
+        Dim testFile As System.IO.FileInfo
+        testFile = My.Computer.FileSystem.GetFileInfo(Txt_Percorso_Anasog_Completa.Text)
+        Dim PathXML As String = testFile.DirectoryName & "\"
+
+        '------------------------------------------------------------------------------------------ Salvataggio dati Mancanti
+        Dim NomeFileSalvataggio As String = PathXML & "SalvaDati_da_" & (testFile.Name).Substring(0, testFile.Name.Length - 4) & ".xml"
+        If My.Computer.FileSystem.FileExists(NomeFileSalvataggio) = True Then
+            My.Computer.FileSystem.DeleteFile(NomeFileSalvataggio)
+        End If
+        Dim Writer As New XmlTextWriter(NomeFileSalvataggio, System.Text.Encoding.UTF8)
+        Writer.WriteStartDocument(True)
+        Writer.Formatting = Formatting.Indented
+        Writer.Indentation = 2
+        Writer.WriteStartElement("Soggetti")
+
+        Do
+            stringReader = FileReader.ReadLine()
+
+            CFoPIVA = stringReader.Substring(160, 16)
+            NoLuogoNascita = stringReader.Substring(498, 6)
+            NoSedeLegale = stringReader.Substring(604, 6)
+            NoSpecieGiuridica = stringReader.Substring(516, 4)
+            NoCodiceCCIA = stringReader.Substring(442, 7)
+
+            SalvaDatiMancanti(CFoPIVA, NoLuogoNascita, NoSedeLegale, NoSpecieGiuridica, NoCodiceCCIA, Writer)
+        Loop Until FileReader.EndOfStream = True
+        Writer.WriteEndDocument()
+        Writer.Close()
+        FileReader.Close()
+
+        Return NomeFileSalvataggio
+    End Function
+
+    Function Correggi_Anagist(ByVal File_XML_DatiMancanti As String) As Boolean
+
+        Dim Anagist_Corretta As Boolean = True  ' Se non ci sono errori o mancanze la correzione sarà eseguita
+
+        Dim FileReader As System.IO.StreamReader
+        Dim FileWriter As System.IO.StreamWriter
+
+        Dim stringReader As String = ""
+        Dim CFoPIVA As String = ""
+        Dim TipoSoggetto As String = ""
+        Dim ContaRecord As Long = 0
+        Dim PercorsoFileNuovo As String = Txt_Percorso_Anagist.Text.Substring(0, Txt_Percorso_Anagist.Text.IndexOf(".")) & "_Corretto.txt"
+        Txt_Percorso_Anagist_Corretto.Text = PercorsoFileNuovo
+        Me.Refresh()
+
+        If My.Computer.FileSystem.FileExists(PercorsoFileNuovo) Then
+            My.Computer.FileSystem.DeleteFile(PercorsoFileNuovo)
+        End If
+        FileReader = My.Computer.FileSystem.OpenTextFileReader(Txt_Percorso_Anagist.Text)
+        FileWriter = My.Computer.FileSystem.OpenTextFileWriter(PercorsoFileNuovo, True)
+        Lbl_Elaborazioni.Text = "Inizio creazione nuovo archivio Anagist corretto."
+        Me.Refresh()
+
+        Dim LuogoNascita As String = ""
+        Dim SedeLegale As String = ""
+        Dim SpecieGiuridica As String = ""
+        Dim CCIA As String = ""
+
+        Dim doc As New Xml.XmlDocument
+        doc.Load(File_XML_DatiMancanti)
+
+        Dim nodes As XmlNodeList = doc.DocumentElement.SelectNodes("/Soggetti/Soggetto")
+        Dim CF_Trovato As Boolean = False
+        Do
+            stringReader = FileReader.ReadLine()
+
+            TipoSoggetto = stringReader.Substring(28, 2)
+            If TipoSoggetto = "PG" Or TipoSoggetto = "PF" Then
+                CFoPIVA = stringReader.Substring(30, 16)                'Codice fiscale o Partita IVA del File Anagist Da correggere.
+                Lbl_CFPIVA_Elaborato.Text = CFoPIVA
+                System.Windows.Forms.Application.DoEvents()
+
+                'Cerco il CFoPIVA nel File XML
+                CF_Trovato = False
+                For Each node As XmlNode In nodes
+
+                    If node.SelectSingleNode("CFoPIVA").InnerText = CFoPIVA Then    'Cerco il CF/PIVA del Record di Anagist
+
+                        LuogoNascita = (node.SelectSingleNode("NoLuogoNascita").InnerText & "000000").Substring(0, 6)
+                        SedeLegale = (node.SelectSingleNode("NoSedeLegale").InnerText.Trim & "000000").Substring(0, 6)
+                        SpecieGiuridica = (node.SelectSingleNode("NoSpecieGiuridica").InnerText & Space(4)).Substring(0, 4)
+                        CCIA = (node.SelectSingleNode("NoCCIA").InnerText & "0000000").Substring(0, 7)
+
+                        CF_Trovato = True
+                        Exit For
+                    End If
+                Next
+
+                If CF_Trovato = False Then
+                    Anagist_Corretta = False
+                    DialogResult = MessageBox.Show("Errore di elaborazione: il CF/PIVA " & CFoPIVA & " non è stato trovato", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Exit Do
+                End If
+
+                Select Case TipoSoggetto
+                    Case "PG"                                                           '*---   Persona Giuridica
+                        'Sostituzione della Sede Legale tramite ricerca in file XML
+                        stringReader = stringReader.Substring(0, 309) & SedeLegale & stringReader.Substring(315, stringReader.Length - 315)
+                        'Sostituzione della Specie Giuridica tramite ricerca in file XML
+                        stringReader = stringReader.Substring(0, 350) & SpecieGiuridica & stringReader.Substring(354, stringReader.Length - 354)
+                        'Sostituzione del CCIA tramite ricerca in file XML
+                        stringReader = stringReader.Substring(0, 335) & CCIA & stringReader.Substring(342, stringReader.Length - 342)
+                    Case "PF"                                                           '*---   Persona Fisica
+                        'Sostituzione del LuogoNascita tramite ricerca in file XML
+                        stringReader = stringReader.Substring(0, 294) & LuogoNascita & stringReader.Substring(300, stringReader.Length - 300)
+                        'Sostituzione della Sede Legale tramite ricerca in file XML
+                        stringReader = stringReader.Substring(0, 309) & SedeLegale & stringReader.Substring(315, stringReader.Length - 315)     'In questo caso è il luogo residenza
+                End Select
+
+            End If
+
+            'Scrivo la riga letta sul nuovo File di Anagist
+            FileWriter.WriteLine(stringReader)
+            ContaRecord += 1
+
+        Loop Until FileReader.EndOfStream = True
+
+        FileReader.Close()
+        FileWriter.Close()
+
+        Return Anagist_Corretta
+    End Function
+
+    Private Sub SalvaDatiMancanti(ByVal CFoPIVA As String, ByVal NoLuogoNascita As String, ByVal NoSedeLegale As String, ByVal NoSpecieGiuridica As String, ByVal NoCodiceCCIA As String, ByVal writer As XmlTextWriter)
+
+        writer.WriteStartElement("Soggetto")
+        writer.WriteStartElement("CFoPIVA")
+        writer.WriteString(CFoPIVA)
+        writer.WriteEndElement()
+        writer.WriteStartElement("NoLuogoNascita")
+        writer.WriteString(NoLuogoNascita)
+        writer.WriteEndElement()
+        writer.WriteStartElement("NoSedeLegale")
+        writer.WriteString(NoSedeLegale)
+        writer.WriteEndElement()
+        writer.WriteStartElement("NoSpecieGiuridica")
+        writer.WriteString(NoSpecieGiuridica)
+        writer.WriteEndElement()
+        writer.WriteStartElement("NoCCIA")
+        writer.WriteString(NoCodiceCCIA)
+        writer.WriteEndElement()
+        writer.WriteEndElement()
+
+    End Sub
 
 End Class
